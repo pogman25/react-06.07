cb37c2e5ddf97523920091422c8b888ef4f49882
Anastassiya <yamorzhinka@gmail.com>
2020-07-08 09:12:21 +0300

    lesson 1

---
+++ src/Message.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+import Styles from './styles/App.css';
+
+
+
+
+
+const Message = ({name}) => {
+  return (
+    <div>
+
+    </div>
+  )
+
+};
+
+export default Message

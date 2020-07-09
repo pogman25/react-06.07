6b29ffb11a8bc1118cc82ccc15f67377649de748
Kseniia <kseniia.pomortseva@gmail.com>
2020-07-09 21:44:20 +0500

    lesson 1

---
+++ Pomortseva-Kseniia/src/Button.jsx
@@ -0,0 +1,25 @@
+import React, {useState} from 'react'
+
+const Button = () => {
+    const [text, setText] = useState('')
+    const [messages, setMessages] = useState([])
+
+    const textChange = (event) => {
+        setText(event.target.value)
+    }
+
+    const addMessage = () => {
+        setMessages([...messages, text])
+        setText('')
+    }
+
+    return <div>
+                <input type='text' value={text} onChange={textChange} />
+                <button onClick={addMessage}>Add</button>
+                {messages.map((message, index) =>
+                    <div key={index}>{message}</div>
+                )}
+        </div>
+}
+
+export default Button
---
+++ Pomortseva-Kseniia/src/index.jsx
@@ -0,0 +1,9 @@
+import React, {Component} from "react";
+import ReactDOM from "react-dom";
+import Button from "./Button";
+
+class HelloMessage extends Component {
+  render() {
+    return <Button />
+  }
+}

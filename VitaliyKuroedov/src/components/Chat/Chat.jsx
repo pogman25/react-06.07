73492c0be7930a08cfdd4a2eaee6d611cc37d3a2
Rastrapon <58354560+Rastraponovich@users.noreply.github.com>
2020-07-12 22:43:30 +0300

    KuroeDov_vitaliy lesson2

--- VitaliyKuroedov/src/components/Chat/Chat.jsx
+++ VitaliyKuroedov/src/components/Chat/Chat.jsx
@@ -2,17 +2,52 @@ import React, {Component, Fragment} from 'react'
 import Message from '../Message/Message'
 
 export default class Chat extends Component {
+    constructor(props){
+        super(props)
+    }
 
     state = {
-        chats: [],
-        input: ''
+        input: '',
+        timeoutId: null,
+        chats: this.props.chats
     }
-    handleSendMessage = (value) => {
-        let id = this.state.chats.length
+
+    handleSyncChat = () => {
+        this.state.chats = this.props.chats
+    }
+
+    handleBotMessage = () => {
+        
+        const currentMessage = this.props.chats
+        const lastMessage = currentMessage[currentMessage.length -1]
+        console.log(currentMessage, 'currentMessage?')
+        console.log(lastMessage, 'lastmessage?')
+
+        if (lastMessage && lastMessage.name == 'я') {
+            clearTimeout(this.state.timeoutId)
+            this.state.timeoutId = setTimeout(() => this.handleSendMessage({
+                text: `Не приставай ко мне, я Бот-Компот`, 
+                name: 'бот-компот', 
+            }), 1000)
+        }
+    }
+
+    handleSendMessage = ({value}) => {
+        let content = {name: 'я', text: this.state.input}
+        
+        if (value) {
+            content = value
+        }
+
+        let id = this.props.chats.length
         if (this.state.input != '') {
-            this.setState({chats: [...this.state.chats,
-                id = {name: "я", text: this.state.input}]})
+             this.props.addMessage(content)
+            
             this.setState({input : ''})
+            if(id > 0) {
+                this.handleBotMessage()
+
+            }
         }
     }
 
@@ -24,7 +59,6 @@ export default class Chat extends Component {
     
     handleChange = (event) => {
         this.setState({ input: event.target.value })
-        
     }
     
     render(){
@@ -32,7 +66,7 @@ export default class Chat extends Component {
             <Fragment>
                 <section className="chat-container">
                     <div className="chat-list">
-                        {this.state.chats.map((message, index) => ( <Message message={message} key={index} /> ))}
+                        {this.props.chats.map((message, index) => ( <Message message={message} key={index} /> ))}
                     </div>
                     <div className="chat-actions">
                         <input 
@@ -43,7 +77,9 @@ export default class Chat extends Component {
                             />
                         <button 
                             className="chat-action__button"
-                            onClick={this.handleSendMessage}>Жмак</button>
+                            onClick={this.handleSendMessage}>
+                            Жмак
+                        </button>
                     </div>
                 </section>
             </Fragment>

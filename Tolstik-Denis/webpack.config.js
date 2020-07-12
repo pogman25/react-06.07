4e4555d0648e59e2beae9b8869894b4e119f4540
denis <dionmagnus@yandex.ru>
2020-07-12 20:47:48 +0300

    Improved lesson 01

--- Tolstik-Denis/webpack.config.js
+++ Tolstik-Denis/webpack.config.js
@@ -9,6 +9,21 @@ module.exports = {
 	path: path.resolve(__dirname, "dist"),
     filename: "[name].[hash:6].js"
   },
+  resolve: {extensions: ['js', 'jsx']},
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      }
+    ]
+  },
   plugins: [
 	  new HtmlWebpackPlugin({
 		  filename: "index.html",

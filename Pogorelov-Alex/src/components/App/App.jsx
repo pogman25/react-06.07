d8790014ecd9bc9f62af96ffbadad7f7279b1d40
Alexandr Pogorelov <sapogi21@yandex.ru>
2020-07-16 20:36:01 +0300

    layouting

--- Pogorelov-Alex/src/components/App/App.jsx
+++ Pogorelov-Alex/src/components/App/App.jsx
@@ -1,12 +1,25 @@
 import React, { Component } from 'react';
 import { v4 as uuidv4 } from 'uuid';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { createMuiTheme, ThemeProvider, withStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Messages from '../Messages';
 import FormMessage from '../FormMessage';
-import Dashboard from '../Dashboard';
+import Header from '../Header';
+import ChatList from '../ChatsList';
 
 const theme = createMuiTheme();
+
+const styles = {
+  content: {
+    flexGrow: 1,
+    height: '100vh',
+    overflow: 'auto',
+    marginTop: theme.spacing(9),
+  },
+  root: {
+    display: 'flex',
+  },
+};
 
 class App extends Component {
   state = {
@@ -38,16 +51,22 @@ class App extends Component {
 
   render() {
     const { messages } = this.state;
+    const { classes } = this.props;
 
     return (
       <ThemeProvider theme={theme}>
         <CssBaseline />
-        <Dashboard />
-        <Messages messages={messages} />
-        <FormMessage addMessage={this.addMessage} />
+        <div className={classes.root}>
+          <Header />
+          <ChatList />
+          <main className={classes.content}>
+            <Messages messages={messages} />
+            <FormMessage addMessage={this.addMessage} />
+          </main>
+        </div>
       </ThemeProvider>
     );
   }
 }
 
-export default App;
+export default withStyles(styles)(App);

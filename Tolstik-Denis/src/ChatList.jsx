d5f68afd96c2c70f2ab1111e7850591c801a9dd8
dionmagnus <dionmagnus@yandex.ru>
2020-07-16 16:14:55 +0300

    lesson03

---
+++ Tolstik-Denis/src/ChatList.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = {
+    
+};
+
+class ChatList extends Component {
+    render() {
+        return (
+            <div></div>
+        );
+    }
+}
+
+export default withStyles()(ChatList);

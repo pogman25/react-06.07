40637a000b3816e47e4bd9e77fa32d9bfc5aac6b
Konfeta <Vale@iMac.local>
2020-07-16 15:57:47 +0300

    Домашнее задание к Уроку 3

---
+++ Valentina Kunina/src/components/FormMessage/FormMessage.jsx
@@ -0,0 +1,76 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import TextField from "@material-ui/core/TextField";
+import { Icon } from "@material-ui/core";
+import SendIcon from '@material-ui/icons/Send';
+import Button from "@material-ui/core/Button";
+import { withStyles, makeStyles } from "@material-ui/core/styles";
+
+const styles = () => {
+    return makeStyles((theme) => ({
+        button: {
+            margin: theme.spacing(1),
+        },
+    }));
+};
+
+class FormMessage extends Component {
+    state = {
+        author: "",
+        text: "",
+    };
+
+    onSubmit = (event) => {
+        const { addMessage } = this.props;
+        event.preventDefault();
+        addMessage(this.state);
+        this.setState({ text: "" });
+    };
+
+    onChange = ({ target }) => {
+        const { value, name } = target;
+        this.setState({ [name]: value });
+    };
+
+    render() {
+        const { author, text } = this.state;
+        const { classes } = this.props;
+
+        return (
+            <form onSubmit={this.onSubmit}>
+                <TextField
+                    name="author"
+                    placeholder="Add your name"
+                    label="Author"
+                    variant="outlined"
+                    value={author}
+                    onChange={this.onChange}
+                />
+                <TextField
+                    name="text"
+                    label="Message"
+                    placeholder="Add your message"
+                    multiline
+                    variant="outlined"
+                    value={text}
+                    onChange={this.onChange}
+                />
+                <Button
+                    variant="contained"
+                    color="primary"
+                    className={classes.button}
+                    endIcon={<SendIcon />}
+                >
+                    Send
+                </Button>
+                {/* <button type="submit">Add Message</button> */}
+            </form>
+        );
+    }
+}
+
+FormMessage.propTypes = {
+    addMessage: PropTypes.func,
+};
+
+export default withStyles(styles)(FormMessage);

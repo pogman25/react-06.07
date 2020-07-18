b5b64d7cf3660aedfc0ea7df4f97a96c2c0a5087
Laimis <62464661+Laimik@users.noreply.github.com>
2020-07-18 15:24:52 +0300

    Add files via upload

--- Ivanov-Laimis/src/components/App/App.jsx
+++ Ivanov-Laimis/src/components/App/App.jsx
@@ -1,67 +1,20 @@
-import React, { Component } from "react";
-import { v4 as uuidv4 } from "uuid";
-import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
-import CssBaseline from "@material-ui/core/CssBaseline";
-import Messages from "../Messages";
- import FormMessage from "../FormMessage";
-//import FormMessage from "../Orders";
-import Dashboard from "../Dashboard";
-//import Layout from "../Layout"
-
+import React from 'react';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { BrowserRouter } from 'react-router-dom';
+import RootRouter from '../../pages/RootRouter/RootRouter';
 
 const theme = createMuiTheme();
 
-class App extends Component {
-  state = {
-    messages: [
-      { author: "user", text: "привет", id: uuidv4() },
-      { author: "user", text: "как дела", id: uuidv4() },
-    ],
-  };
-  timer = null;
-
-  componentDidUpdate() {
-    const { messages } = this.state;
-    clearTimeout(this.timer);
-    if (messages[messages.length - 1].author !== "bot") {
-      this.timer = setTimeout(() => {
-        this.setState(({ messages }) => ({
-          messages: [
-            ...messages,
-            { author: "bot", text: "привет от бота", id: uuidv4() },
-          ],
-        }));
-      }, 1000);
-    }
-  }
- 
-  addMessage = ({ author, text }) => {
-    this.setState(({ messages }) => ({
-      messages: [...messages, { author, text, id: uuidv4() }],
-    }));
-  };
-
-  render() {
-    const { messages } = this.state;
-
-    return (
+const App = () => {
+  return (
+    <BrowserRouter>
       <ThemeProvider theme={theme}>
-
-         <div className="layout"> 
         <CssBaseline />
-        
-
-        <Dashboard />  
-        <Messages messages={messages} />
-        <FormMessage addMessage={this.addMessage} />
-        
-        
-        
-        </div>
-
+        <RootRouter />
       </ThemeProvider>
-    );
-  }
-}
+    </BrowserRouter>
+  );
+};
 
 export default App;

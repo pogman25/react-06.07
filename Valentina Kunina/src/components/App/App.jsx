d737e2dd484be5b62185331fc38acd9c4e6ea038
Konfeta <Vale@iMac.local>
2020-07-18 13:16:16 +0300

    Добавление стилей

--- Valentina Kunina/src/components/App/App.jsx
+++ Valentina Kunina/src/components/App/App.jsx
@@ -1,62 +1,65 @@
 import React, { PureComponent } from "react";
+import { BrowserRouter } from "react-router-dom";
 import shortid from "shortid";
 import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Messages from "../Messages";
 import FormMessage from "../FormMessage";
-import Menu from "../Menu";
+import Layout from "../Layout";
 
 const theme = createMuiTheme();
 
 class App extends PureComponent {
-    state = {
-        messages: [
-            { author: "user", text: "привет", id: shortid.generate() },
-            { author: "user", text: "как дела?", id: shortid.generate() },
-        ],
-    };
+  state = {
+    messages: [
+      { author: "user", text: "привет", id: shortid.generate() },
+      { author: "user", text: "как дела?", id: shortid.generate() },
+    ],
+  };
 
-    timer = null;
+  timer = null;
 
-    componentDidUpdate(prevProps, prevState) {
-        const { messages } = this.state;
-        clearTimeout(this.timer);
+  componentDidUpdate() {
+    const { messages } = this.state;
+    clearTimeout(this.timer);
 
-        if (messages[messages.length - 1].author !== "bot") {
-            this.timer = setTimeout(() => {
-                this.setState(({ messages }) => ({
-                    messages: [
-                        ...messages,
-                        {
-                            author: "bot",
-                            text: "Привет от Бота",
-                            id: shortid.generate(),
-                        },
-                    ],
-                }));
-            }, 1000);
-        }
-    }
-
-    addMessage = ({ author, text, id = shortid.generate() }) => {
-        this.setState(({ messages }) => ({
-            messages: [...messages, { author, text, id }],
+    if (messages[messages.length - 1].author !== "bot") {
+      this.timer = setTimeout(() => {
+        this.setState(({ messages: currentMessages }) => ({
+          messages: [
+            ...currentMessages,
+            {
+              author: "bot",
+              text: "Привет от Бота",
+              id: shortid.generate(),
+            },
+          ],
         }));
-    };
-
-    render() {
-        const { messages } = this.state;
-        // const { name } = this.props;
-
-        return (
-            <ThemeProvider theme={theme}>
-                <CssBaseline />
-                <Menu />
-                <Messages messages={messages} />
-                <FormMessage addMessage={this.addMessage} />
-            </ThemeProvider>
-        );
+      }, 1000);
     }
+  }
+
+  addMessage = ({ author, text, id = shortid.generate() }) => {
+    this.setState(({ messages }) => ({
+      messages: [...messages, { author, text, id }],
+    }));
+  };
+
+  render() {
+    const { messages } = this.state;
+
+    return (
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Layout>
+            <Messages messages={messages} />
+            <FormMessage addMessage={this.addMessage} />
+          </Layout>
+        </ThemeProvider>
+      </BrowserRouter>
+    );
+  }
 }
 
 // HelloMessage.propTypes = {

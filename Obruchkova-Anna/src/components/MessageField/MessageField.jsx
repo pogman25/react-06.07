d3fafea703762e17488129c22d8ca6d48a69ef08
Anna <anny9393@list.ru>
2020-07-18 16:52:41 +1000

    Adding Homework_2

--- Obruchkova-Anna/src/components/MessageField/MessageField.jsx
+++ Obruchkova-Anna/src/components/MessageField/MessageField.jsx
@@ -1,30 +1,31 @@
 import React, { Component } from 'react';
 import { Message } from '../Message/Message';
-import { FormMessage } from '../FormMessage/FormMessage'
+import { FormMessage } from '../FormMessage/FormMessage';
+import { v4 as uuidv4 } from 'uuid';
 
 export class MessageField extends Component {
 
     state = {
         messages: [
-            { user: 'User', text: 'Hello' },
-            { user: 'User2', text: 'How are you' },
+            { user: 'User', text: 'Hello', id: uuidv4() },
+            { user: 'User2', text: 'How are you', id: uuidv4() },
         ],
     };
 
-    componentDidUpdate(prevProps, prevState) {
+    componentDidUpdate() {
         const { messages } = this.state;
         if (messages[messages.length - 1].user !== 'Robot') {
             setTimeout(() => {
                 this.setState(({ messages }) => ({
-                    messages: [...messages, { user: 'Robot', text: 'I am Robot' }]
+                    messages: [...messages, { user: 'Robot', text: 'I am Robot', id: uuidv4()}]
                 }));
             }, 1000)
         }
     }
-    sendMessage = (message) => {
+    sendMessage = ({user, text}) => {
         const { messages } = this.state;
         this.setState(({ messages }) => ({
-            messages: [...messages, message],
+            messages: [...messages, {user, text, id: uuidv4()}],
 
         }));
     };

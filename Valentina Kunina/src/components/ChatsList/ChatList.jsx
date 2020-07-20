c08f95ecd5d48ef245faa1cc54875e29c6fc7012
Konfeta <Vale@iMac.local>
2020-07-20 15:47:38 +0300

    WIP Домашнее задание к Уроку 4

---
+++ Valentina Kunina/src/components/ChatsList/ChatList.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import cx from "classnames";
+import PropTypes from "prop-types";
+import Drawer from "@material-ui/core/Drawer";
+import Divider from "@material-ui/core/Divider";
+import IconButton from "@material-ui/core/IconButton";
+import ChevronLeftIcon from "@material-ui/icons/ChevronLeft";
+import useStyles from "../Layout/useStyles";
+
+const ChatList = ({ open, handleDrawerClose, children }) => {
+  const classes = useStyles();
+  return (
+    <Drawer
+      variant="permanent"
+      classes={{
+        paper: cx(classes.drawerPaper, !open && classes.drawerPaperClose),
+      }}
+      open={open}
+    >
+      <div className={classes.toolbarIcon}>
+        <IconButton onClick={handleDrawerClose}>
+          <ChevronLeftIcon />
+        </IconButton>
+      </div>
+      <Divider />
+      {children}
+    </Drawer>
+  );
+};
+
+ChatList.propTypes = {
+  open: PropTypes.bool.isRequired,
+  handleDrawerClose: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+export default ChatList;

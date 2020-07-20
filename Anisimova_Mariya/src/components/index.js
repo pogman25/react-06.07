2e022b8d46c75f8b82bf3208f45e73a56108b3ff
marmaran <xnewlab@gmail.com>
2020-07-20 12:47:53 +0300

    hw 3

---
+++ Anisimova_Mariya/src/components/index.js
@@ -0,0 +1,4 @@
+export {default as Layout} from './Layout'
+export {default as ChatList} from './ChatList'
+export {default as MessageField} from './MessageField'
+export {default as Header} from './Header'

1e01acccb57f14397ec2d7ab027b623929907cda
Kolya <bazlovrabota@gmail.com>
2020-07-23 11:25:37 +0300

    151

---
+++ Ivanov-Laimis/src/reducers/profileReducer.js
@@ -0,0 +1,14 @@
+import { handleActions } from 'redux-actions';
+import { userData } from '../actions/profile';
+
+const initialStore = {
+    num: 'инфа',
+};
+
+
+ const profileReducer = initialStore;
+
+
+export default profileReducer;
+
+

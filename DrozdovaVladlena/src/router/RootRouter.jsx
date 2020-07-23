de421c126c134b07c57d352de12d28d602662e39
drozd-v <drozdova_vl03@gmail.com>
2020-07-23 13:17:22 +0300

    redux, react-redux

--- DrozdovaVladlena/src/router/RootRouter.jsx
+++ DrozdovaVladlena/src/router/RootRouter.jsx
@@ -1,16 +1,27 @@
-import React from 'react';
-import { Layout, Chats } from '../components';
-import Profile from '../components/Profile';
-import { Route,  Switch } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { Route, Switch, Redirect } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { Chats } from '../components';
+import { Profile } from '../components';
+import mockChats from '../components/mock/mockChats';
+import { getChatsSuccess } from '../store/actions/chatsAction';
+import { getMessagesSuccess } from '../store/actions/messageAction';
+import mockMessage from '../components/mock/mockMessage';
 
 const RootRouter = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getChatsSuccess(mockChats));
+    dispatch(getMessagesSuccess(mockMessage));
+  }, [dispatch]);
+
   return (
-    <Layout>
       <Switch>
-          <Route exact path='/profile' component={Profile} />
-          <Route path='/chat/:chatId' component={Chats} />
+        <Redirect exact from='/' to='/profile' />
+        <Route path="/profile" component={Profile} />
+        <Route path="/chat/:chatId" component={Chats} />
       </Switch>
-    </Layout>
   );
 };
 

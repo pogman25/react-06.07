02771718b9060b1646d73f5e9eec76aa5d0682e1
Alexandr Pogorelov <sapogi21@yandex.ru>
2020-07-23 21:59:43 +0300

    add persist

--- Pogorelov-Alex/src/store/index.js
+++ Pogorelov-Alex/src/store/index.js
@@ -1,9 +1,17 @@
 import { createStore, applyMiddleware, compose } from 'redux';
 import logger from 'redux-logger';
 import thunk from 'redux-thunk';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import rootReducer from '../reducers';
-import { saveState, loadState } from './localStorage';
 // import botAnswer from './botAnswer';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 
 const composeEnhancers =
   typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
@@ -17,12 +25,8 @@ const enhancer = composeEnhancers(
   // other store enhancers if any
 );
 
-const persistedState = loadState();
-
-const store = createStore(rootReducer, persistedState, enhancer);
-
-store.subscribe(() => {
-  saveState(store.getState());
-});
-
-export default store;
+export default () => {
+  const store = createStore(persistedReducer, enhancer);
+  const persistor = persistStore(store);
+  return { store, persistor };
+};

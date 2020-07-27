5407c80c9de8d505caaa7c60cda8802314dd0ef9
marmaran <xnewlab@gmail.com>
2020-07-27 15:01:51 +0300

    hw5

---
+++ Anisimova_Mariya/src/store/selector/selector.js
@@ -0,0 +1,19 @@
+export const getPageChat = (store, chatId) => {
+    const currentChat = store.chats.byIds[chatId];
+    if (currentChat) {
+      return currentChat;
+    }
+    return {
+      title: '',
+    };
+  };
+  
+  export const getAllChats = ({ chats }) => {
+    const { ids, byIds } = chats;
+    return ids.map(id => byIds[id]);
+  };
+  
+  
+  export const getMessages = (store, chatId) => {
+    return store.messages.byIds[chatId]
+  }

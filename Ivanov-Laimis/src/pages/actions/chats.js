530c4e6da739f4430dee0b3fe0565fb92d3c4d43
Laimis <62464661+Laimik@users.noreply.github.com>
2020-07-29 18:18:10 +0300

    Add files via upload

---
+++ Ivanov-Laimis/src/pages/actions/chats.js
@@ -0,0 +1,71 @@
+import { createAction } from 'redux-actions';
+import { v4 as uuidv4 } from 'uuid';
+import { BOT_NAME } from '../utils/constants';
+import { getFullName } from '../selectors/profile';
+
+// simple actions
+
+export const chatsRequest = createAction('chats/request');
+export const getChatsSuccess = createAction('chats/GET_CHATS_SUCCESS');
+export const getChatsFailure = createAction('chats/GET_CHATS_FAILURE');
+export const getChatsEnd = createAction('chats/GET_CHATS_END');
+export const saveMessage = createAction('chats/ADD_MESSAGE');
+
+export const getMessagesSuccess = createAction('messages/GET_MESSAGES_SUCCESS');
+
+export const addUpdatedMessage = createAction('chats/ADD_UPDATED_MESSAGE');
+export const deleteUpdatedMessage = createAction('chats/DELETE_UPDATED_MESSAGE');
+
+// smart actions
+
+export const addMessage = data => (dispatch, getState) => {
+  const fullName = getFullName(getState());
+  if (data.message.author !== BOT_NAME) {
+    setTimeout(() => {
+      dispatch(
+        saveMessage({
+          chatId: data.chatId,
+          message: { author: BOT_NAME, text: 'привет, я Бот', id: uuidv4() },
+        }),
+      );
+    }, 500);
+  }
+
+  dispatch(
+    saveMessage({
+      chatId: data.chatId,
+      message: { author: fullName, ...data.message },
+    }),
+  );
+};
+
+export const sendChatsRequest = () => async dispatch => {
+  dispatch(chatsRequest());
+
+  // async/await
+  try {
+    const res = await fetch('/api/chats.json');
+    const data = await res.json();
+    dispatch(getMessagesSuccess(data.messages));
+    dispatch(getChatsSuccess(data.chats));
+  } catch (e) {
+    dispatch(getChatsFailure());
+  } finally {
+    dispatch(getChatsEnd());
+  }
+
+  // Promiss
+  // fetch('/api/chats.json')
+  //   .then(res => res.json())
+  //   .then(res => {
+  //     console.log(res);
+  //     dispatch(getMessagesSuccess(res.messages));
+  //     dispatch(getChatsSuccess(res.chats));
+  //   })
+  //   .catch(e => {
+  //     dispatch(getChatsFailure());
+  //   })
+  //   .finally(() => {
+  //     dispatch(getChatsEnd());
+  //   });
+};

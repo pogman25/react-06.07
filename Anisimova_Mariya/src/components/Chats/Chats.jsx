469d91333c553107bbbe7988a19af1f1e09e1be5
marmaran <xnewlab@gmail.com>
2020-07-30 14:58:07 +0300

    hw6

--- Anisimova_Mariya/src/components/Chats/Chats.jsx
+++ Anisimova_Mariya/src/components/Chats/Chats.jsx
@@ -1,39 +1,25 @@
-  
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
-import uuid from 'react-uuid';
 import Layout from '../Layout';
 import Messages from './Messages';
 import MessageField from './MessageField/MessageField';
 import { getMessages } from '../../store/selector/selector';
-import { botAnswerSuccess, addMessageUser } from '../../store/actions/messageAction';
+import { addMessageChat } from '../../store/actions/chatsAction';
 
 const Chats = () => {
-  const [botTell, setBotTell] = useState(false)
   const {chatId} = useParams();
   const messageList = useSelector(store => getMessages(store, chatId))
   const dispatch = useDispatch()
 
-  useEffect(() => {
-    if (botTell) {
-      const id = uuid();
-      setTimeout(() => {
-        dispatch(botAnswerSuccess(id, chatId))
-        setBotTell(false);
-      }, 1000);
-    }
-  }, [botTell]);
-
   const addMessage = (message) => {
-    dispatch(addMessageUser(message, chatId))
-    setBotTell(true);
+    dispatch(addMessageChat(message, chatId))
   };
 
   return (
     <Layout>
       <Messages messages={messageList} />
-      <MessageField addMessage={addMessage} botTell={botTell} />
+      <MessageField addMessage={addMessage}/>
     </Layout>
   );
 };
